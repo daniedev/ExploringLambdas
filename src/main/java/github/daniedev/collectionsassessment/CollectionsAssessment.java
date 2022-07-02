f8979a3c975c744a1d2d4c645f878381193a3a46
daniedev <a.danielpf21@gmail.com>
2022-07-02 15:27:21 +0530

    Solution using Java 7

--- src/main/java/github/daniedev/collectionsassessment/CollectionsAssessment.java
+++ src/main/java/github/daniedev/collectionsassessment/CollectionsAssessment.java
@@ -1,7 +1,6 @@
 package github.daniedev.collectionsassessment;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -17,17 +16,42 @@ public class CollectionsAssessment {
         );
 
         //Task1: Sort list by last name
-
-
+        people.sort(new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getLastName().compareTo(o2.getLastName());
+            }
+        });
 
 
         //Task2: Create a method that prints all elements in the list
-
-
+        System.out.println("Printing all names");
+        printAllNames(people);
 
 
         //Task3: Create a method that prints all people with last name beginning with C
+        System.out.println("Printing people's last name beginning with C");
+        printNamesConditionally(people, new Condition() {
+            @Override
+            public boolean isValid(Person p) {
+                return p.getLastName().startsWith("C");
+            }
+        });
 
 
+    }
+
+
+    private static void printAllNames(List<Person> people) {
+        for (Person p : people) {
+            System.out.println(p);
+        }
+    }
+
+    private static void printNamesConditionally(List<Person> people, Condition condition) {
+        for (Person p : people) {
+            if (condition.isValid(p))
+                System.out.println(p);
+        }
     }
 }
---
+++ src/main/java/github/daniedev/collectionsassessment/Condition.java
@@ -0,0 +1,5 @@
+package github.daniedev.collectionsassessment;
+
+public interface Condition {
+    boolean isValid(Person p);
+}
